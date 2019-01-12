889812fb5d5e10880a2373a3c3f82acbbeece8f2
Norbertas <norbiuxx@gmail.com>
2019-01-12 11:28:18 +0200

    first commit

---
+++ Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Header = () => {
+    const{textStyle, viewStyle} = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    )
+}
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        allignItems: 'center',
+        height: 70,
+        paddingTop: 30,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative',
+
+    },
+    textStyle: {
+        fontSize: 25
+    }
+}
+
+export default Header;
